8e58e3d67b6160ed9e697a5b1e48bc1f3a27c0de
Tobias-Czermak <158488669+Tobias-Czermak@users.noreply.github.com>
2025-06-05 14:32:26 +0200

    Add files via upload

--- Code website/register.php
+++ Code website/register.php
@@ -5,18 +5,18 @@
     <title>Registrieren</title>
 </head>
 <body>
-<h2>Neuen Benutzer registrieren</h2>
-<form action="register_submit.php" method="post">
-    <label for="email">E-Mail:</label><br>
-    <input type="email" name="email" required><br><br>
+    <h2>Neuen Benutzer registrieren</h2>
+    <form action="register_submit.php" method="post">
+        <label for="email">E-Mail:</label><br>
+        <input type="email" name="email" required><br><br>
 
-    <label for="password">Passwort:</label><br>
-    <input type="password" name="password" required><br><br>
+        <label for="password">Passwort:</label><br>
+        <input type="password" name="password" required><br><br>
 
-    <label for="confirm_password">Passwort wiederholen:</label><br>
-    <input type="password" name="confirm_password" required><br><br>
+        <label for="confirm_password">Passwort wiederholen:</label><br>
+        <input type="password" name="confirm_password" required><br><br>
 
-    <input type="submit" value="Registrieren">
-</form>
+        <input type="submit" value="Registrieren">
+    </form>
 </body>
 </html>
